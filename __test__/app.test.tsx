28c10dd9fca4e61592ac43254b2f5076084baf62
Sten Henriksson <sten.henriksson@visma.com>
2023-02-05 23:45:20 +0100

    first commit

---
+++ __test__/app.test.tsx
@@ -0,0 +1,10 @@
+
+import { beforeEach, describe, expect, test } from 'vitest';
+import { App } from "../src/app"
+import { render, fireEvent, screen, waitFor } from '@testing-library/preact';
+
+
+test('should display initial count', () => {
+  const { container } = render(<App></App>);
+  expect(container.textContent).toMatch('Click on the Vite and Preact logos to learn more');
+});
